a6a57b1cdd8381388b4b229abd5440b49908c787
RayElProOficial <88939475+RayElProOficial@users.noreply.github.com>
2023-08-27 12:34:19 +0200

    Move all bootcode to RaxOS.Boot

--- RaxOS/Kernel.cs
+++ RaxOS/Kernel.cs
@@ -1,4 +1,5 @@
 ﻿using Cosmos.System.Graphics;
+using IL2CPU.API.Attribs;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
@@ -11,37 +12,12 @@ namespace RaxOS
     {
         Canvas canvas;
 
-        private readonly Bitmap bitmap = new Bitmap(10, 10,
-                new byte[] { 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0,
-                    255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255,
-                    0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255,
-                    0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 23, 59, 88, 255,
-                    23, 59, 88, 255, 0, 255, 243, 255, 0, 255, 243, 255, 23, 59, 88, 255, 23, 59, 88, 255, 0, 255, 243, 255, 0,
-                    255, 243, 255, 0, 255, 243, 255, 23, 59, 88, 255, 153, 57, 12, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255,
-                    243, 255, 0, 255, 243, 255, 153, 57, 12, 255, 23, 59, 88, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243,
-                    255, 0, 255, 243, 255, 0, 255, 243, 255, 72, 72, 72, 255, 72, 72, 72, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0,
-                    255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 72, 72,
-                    72, 255, 72, 72, 72, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255,
-                    10, 66, 148, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255,
-                    243, 255, 10, 66, 148, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 10, 66, 148, 255, 10, 66, 148, 255,
-                    10, 66, 148, 255, 10, 66, 148, 255, 10, 66, 148, 255, 10, 66, 148, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255,
-                    243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 10, 66, 148, 255, 10, 66, 148, 255, 10, 66, 148, 255, 10, 66, 148,
-                    255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255,
-                    0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, }, ColorDepth.ColorDepth32);
+        
 
         protected override void BeforeRun()
         {
-            // If all works correctly you should not really see this :-)
             Console.WriteLine("Cosmos booted successfully. Let's go in Graphical Mode");
-
-            /*
-            You don't have to specify the Mode, but here we do to show that you can.
-            To not specify the Mode and pick the best one, use:
-            canvas = FullScreenCanvas.GetFullScreenCanvas();
-            */
             canvas = FullScreenCanvas.GetFullScreenCanvas(new Mode(1920, 1080, ColorDepth.ColorDepth32));
-
-            // This will clear the canvas with the specified color.
             canvas.Clear(Color.Blue);
         }
 
@@ -49,29 +25,7 @@ namespace RaxOS
         {
             try
             {
-                // A red Point
-                canvas.DrawPoint(Color.Red, 69, 69);
-
-                // A GreenYellow horizontal line
-                canvas.DrawLine(Color.GreenYellow, 250, 100, 400, 100);
-
-                // An IndianRed vertical line
-                canvas.DrawLine(Color.IndianRed, 350, 150, 350, 250);
-
-                // A MintCream diagonal line
-                canvas.DrawLine(Color.MintCream, 250, 150, 400, 250);
-
-                // A PaleVioletRed rectangle
-                canvas.DrawRectangle(Color.PaleVioletRed, 350, 350, 80, 60);
-
-                // A LimeGreen rectangle
-                canvas.DrawRectangle(Color.LimeGreen, 450, 450, 80, 60);
-
-                // A bitmap
-                canvas.DrawImage(bitmap, 100, 150);
-
-                canvas.Display(); // Required for something to be displayed when using a double buffered driver
-
+                Boot.BootSys();
                 Console.ReadKey();
                 Sys.Power.Shutdown();
             }
