c363a03753b1b719b5f862866c15f833a294b3ad
RayElProOficial <88939475+RayElProOficial@users.noreply.github.com>
2023-08-27 12:32:43 +0200

    Boot manager

---
+++ RaxOS/Boot.cs
@@ -0,0 +1,30 @@
+﻿using Cosmos.System.Graphics;
+using IL2CPU.API.Attribs;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RaxOS
+{
+    internal class Boot
+    {
+        static Canvas canvas;
+        [ManifestResourceStream(ResourceName = "RaxOS.Images.boot.bmp")]
+        static byte[] byte2;
+        static Bitmap bootlogo = new Bitmap(byte2);
+        public static void BootSys()
+        {
+            canvas = FullScreenCanvas.GetFullScreenCanvas(new Mode(1920, 1080, ColorDepth.ColorDepth32));
+            canvas.Clear(Color.Black);
+            canvas.DrawImage(bootlogo, 660, 240);
+            canvas.Display();
+            //canvas.Disable();
+            MainDesktop.StartDesk();
+        }
+    }
+}
